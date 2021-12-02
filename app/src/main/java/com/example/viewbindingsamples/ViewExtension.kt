4305d02a0065ab0f1175136c4cfe568dff7827d2
Behzod Bozorboev <behzoddev@gmail.com>
2021-12-02 16:20:23 +0900

    Update view extensions

--- app/src/main/java/com/example/viewbindingsamples/ViewExtension.kt
+++ app/src/main/java/com/example/viewbindingsamples/ViewExtension.kt
@@ -1,19 +1,47 @@
 package com.example.viewbindingsamples
 
+
 import android.content.Context
 import android.view.View
 
 import android.widget.Toast
+import androidx.annotation.IdRes
+import androidx.annotation.LayoutRes
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import androidx.viewbinding.ViewBinding
 
+// It shows a toast message via an extension function for activity
 fun Context.toast(message: String) {
     Toast.makeText(this, message, Toast.LENGTH_LONG).show()
 }
+
+fun Fragment.toast(message: String) {
+    Toast.makeText(requireContext(),message,Toast.LENGTH_LONG).show()
+}
+
+// It helps to replace easily fragments in activity
+// https://stackoverflow.com/questions/45713747/any-code-improvement-in-adding-replacing-fragment
+fun AppCompatActivity.replaceFragmentSafely(
+    fragment: Fragment,
+    @IdRes resId: Int
+) {
+    supportFragmentManager.beginTransaction().apply {
+        replace(resId,fragment)
+        addToBackStack(null)
+        commit()
+    }
+}
+
+// It gets a string id
 fun View.getString(resId: Int): String {
     return resources.getString(resId)
 }
+// It shows a visible of view
 fun View.visible() {
     this.visibility = View.VISIBLE
 }
+// It shows a gone of view
 fun View.gone() {
     this.visibility = View.GONE
 }
